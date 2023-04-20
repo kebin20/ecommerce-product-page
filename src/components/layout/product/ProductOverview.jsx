7f5218d3a5987f950f1bfff5950f0d52b1b1a571
kebin20 <ktanzyl@hotmail.com>
2023-04-20 21:55:11 +0900

    chore: added a button container

--- src/components/layout/product/ProductOverview.jsx
+++ src/components/layout/product/ProductOverview.jsx
@@ -1,9 +1,48 @@
-import { ProductDescription } from '../../ExportComponents';
+import React from "react";
+import {
+  ProductDescription,
+  Carousel,
+  Button,
+  iconPlus,
+  iconMinus,
+  iconCart,
+} from "../../ExportComponents";
 
 const ProductOverview = () => {
+
+  function deleteItem() {}
+
+  function addItem() {}
+
+  function addToCart() {}
+
   return (
     <div>
+      <Carousel />
       <ProductDescription />
+      <div className="item-counter">
+        <Button
+          onClick={deleteItem}
+          src={iconMinus}
+          title="Minus Icon"
+          ariaLabel={"Decrement product quantity"}
+        />
+        <span>0</span>
+        <Button
+          onClick={addItem}
+          src={iconPlus}
+          title="Plus Icon"
+          ariaLabel={"Increment product quantity"}
+        />
+      </div>
+      <Button
+        onClick={addToCart}
+        src={iconCart}
+        title="Cart icon"
+        ariaLabel={"add to cart"}
+      >
+        Add to Cart
+      </Button>
     </div>
   );
 };
--- src/components/layout/product/description/ProductDescription.jsx
+++ src/components/layout/product/description/ProductDescription.jsx
@@ -1,4 +1,5 @@
-import './ProductDescription.scss';
+import React from "react";
+import "./ProductDescription.scss";
 
 const ProductDescription = () => {
   return (
