82224fa2a797b4e4f8e0038ffbb72862bc911661
kebin20 <ktanzyl@hotmail.com>
2023-04-20 21:56:35 +0900

    chore: created button component

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <div></div>;
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const Button = ({ src, onClick, title, ariaLabel, className }) => {
+  return (
+      <button className={className} onClick={onClick} aria-label={ariaLabel} title={title}>
+        <img src={src} alt={title} />
+      </button> 
+  );
 };
 
-export default Button;
+
+
+export { Button };
