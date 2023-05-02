6b15cabfe8265b09c88b7147cb9a018ed0d2be9c
kebin20 <ktanzyl@hotmail.com>
2023-05-02 15:45:54 +0900

    style: updated styling for button container

--- src/components/layout/product/ProductOverview.jsx
+++ src/components/layout/product/ProductOverview.jsx
@@ -20,29 +20,31 @@ const ProductOverview = () => {
     <div>
       <Carousel />
       <ProductDescription />
-      <div className="item-counter">
-        <Button
-          onClick={deleteItem}
-          src={iconMinus}
-          title="Minus Icon"
-          ariaLabel={'Decrement product quantity'}
-        />
-        <span className="counter-number">0</span>
-        <Button
-          onClick={addItem}
-          src={iconPlus}
-          title="Plus Icon"
-          ariaLabel={'Increment product quantity'}
-        />
-      </div>
-      <div className="cart-button">
-        <Button
-          onClick={addToCart}
-          src={iconCart}
-          title="Cart icon"
-          ariaLabel={'add to cart'}
-        ></Button>
-        <span>Add to Cart</span>
+      <div className="button-container">
+        <div className="item-counter">
+          <Button
+            onClick={deleteItem}
+            src={iconMinus}
+            title="Minus Icon"
+            ariaLabel={'Decrement product quantity'}
+          />
+          <span className="counter-number">0</span>
+          <Button
+            onClick={addItem}
+            src={iconPlus}
+            title="Plus Icon"
+            ariaLabel={'Increment product quantity'}
+          />
+        </div>
+        <div className="cart-button">
+          <Button
+            onClick={addToCart}
+            src={iconCart}
+            title="Cart icon"
+            ariaLabel={'add to cart'}
+          ></Button>
+          <span>Add to Cart</span>
+        </div>
       </div>
     </div>
   );
